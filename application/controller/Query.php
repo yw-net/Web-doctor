686cb8befc69b725f88b8c30c7137e40f0641b04
fuyouwei <3331977219@qq.com>
2021-07-05 15:36:59 +0800

    Initial commit

--- application/controller/Query.php
+++ application/controller/Query.php
@@ -4,6 +4,8 @@
 namespace app\controller;
 
 
+use think\Db;
+
 class Query extends Base
 {
     //用户中心页面
@@ -16,6 +18,20 @@ class Query extends Base
     //查询按钮
     public function resQuery()
     {
+        $get_data = $this->request->param();
+        Db::query($get_data['data']);
+//        return $get_data['data'][1]['fenlei'];
+
+//            Db::view($get_data['data'][0]['fenlei'], 'patients_id')
+//        for ($i = 0; $i < count($get_data['data']); $i++){
+//                ->view('Profile', 'truename,phone,email', 'Profile.user_id=$get_data['data'][0]['fenlei'].patients_id')
+//                ->view('Score', 'score', 'Score.user_id=Profile.id')
+//                ->where('score', '>', 80)
+//                ->select();
+
+//        }
+
+
 
     }
 
